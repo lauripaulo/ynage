f7457c7e58518bce13dbbde2d9fe9e522f06943d
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-12 19:38:44 -0300

    First draft of a subset of the repository query fields.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class GithubRepository(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+    full_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=2048)
+    fork = models.CharField(max_length=5)
+    url = models.CharField(max_length=1024)
+    created_at = models.DateTimeField()  # 2012-03-30T20:09:52Z
+    homepage = models.CharField(max_length=1024)
+    size = models.BigIntegerField()
+    language = models.CharField(max_length=32)
+    open_issues = models.IntegerField()
+    watchers = models.IntegerField()
