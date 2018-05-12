eae1cda3a1a29fb8a428d850b1904ff9f1685788
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-12 13:34:16 -0300

    Change to local js/css static files. Browse and search page stubs added.

--- home/views.py
+++ home/views.py
@@ -5,6 +5,18 @@ from django.template import loader
 # Create your views here.
 def index(request):
     context = {
-        'test_message': 'This is django!!!'
+        'test_message': 'django index page.'
     } 
     return render(request, 'home/index.html', context)
+
+def search(request):
+    context = {
+        'test_message': 'django search page.'
+    }
+    return render(request, 'homee/search.html', context)
+
+def browse(request):
+    context = {
+        'test_message': 'django browse page.'
+    }
+    return render(request, 'home/browse.html', context)
