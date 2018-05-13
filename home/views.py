b9ab6073be2bd7e1a17015f50c6c8577a4943d5a
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-12 23:25:05 -0300

    Implemented form POST and data flow from index to browse.

--- home/views.py
+++ home/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from home.homelib import RepositoryHandler
+from home.forms import SearchForm
+
 
 # Create your views here.
 def index(request):
+    form = SearchForm()
     context = {
-        'test_message': 'django index page.'
-    } 
+        'test_message': 'django index page.',
+        'form': form
+    }
     return render(request, 'home/index.html', context)
 
-def search(request):
-    context = {
-        'test_message': 'django search page.'
-    }
-    return render(request, 'homee/search.html', context)
 
 def browse(request):
-    context = {
-        'test_message': 'django browse page.'
-    }
+    form = SearchForm(request.POST)
+    if form.is_valid():
+        keyword = request.POST['keyword']
+        language = request.POST['language']
+        context = {
+            'test_message': 'django browse page.',
+            'keyword': keyword,
+            'language': language
+        }
     return render(request, 'home/browse.html', context)
