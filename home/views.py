f97a647e9f8b4bab4a56d52e0f5dc963880dea4b
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-13 01:08:04 -0300

    The browse view with a POST now search in github for the keyword + language, saves the data and add it to the context. If it is accessed with a GET it retrives the data from the db thing.

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from home.homelib import RepositoryHandler
 from home.forms import SearchForm
+from home.homelib import GithubRepository
 
 
 # Create your views here.
@@ -16,13 +17,24 @@ def index(request):
 
 
 def browse(request):
+    keyword = None
+    language = None
+    repository_list = []
     form = SearchForm(request.POST)
-    if form.is_valid():
-        keyword = request.POST['keyword']
-        language = request.POST['language']
-        context = {
-            'test_message': 'django browse page.',
-            'keyword': keyword,
-            'language': language
-        }
+    if request.method == 'POST':
+        if form.is_valid():
+            keyword = request.POST['keyword']
+            language_id = int(request.POST['language']) - 1
+            language = form.languages[language_id][1]
+            ghr = RepositoryHandler()
+            repository_list = ghr.get_remote_repositories(keyword, language)
+    else:
+        ghr = RepositoryHandler()
+        repository_list = ghr.list_saved_repositories()
+    context = {
+        'test_message': 'django browse page.',
+        'keyword': keyword,
+        'language': language,
+        'repository_list': repository_list
+    }
     return render(request, 'home/browse.html', context)
