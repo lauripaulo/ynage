d3e1c8061905528c8f5f50000aa3c825d801447d
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-13 01:06:16 -0300

    Removed search view. No longer used.

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
     path('browse/', views.browse, name='browse')
 ]
