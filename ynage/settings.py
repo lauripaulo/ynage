a157bcf707abaa8a6f62e4e885cd86a29bc895ef
lauripaulo <lauripaulo@gmail.com>
2018-05-14 22:24:26 -0300

    Added local and heroku hosts to host allowed list.

--- ynage/settings.py
+++ ynage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6m(p#cza%ich1a-$&m4ow_nc%@n-sb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ynage.herokuapp.com']
 
 
 # Application definition
