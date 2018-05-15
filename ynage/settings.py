1782fcfbb0ea33b83087757ee013c53a1b226d1e
Lauri Paulo Laux Jr <lauripaulo@gmail.com>
2018-05-14 21:03:06 -0300

    Changes secret and debug mode to use env vars DJANGO_SECRET_KEY and DJANGO_DEBUG.

--- ynage/settings.py
+++ ynage/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6m(p#cza%ich1a-$&m4ow_nc%@n-sb*#mewe6tw(40rer-)du0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6m(p#cza%ich1a-$&m4ow_nc%@n-sb*#mewe6tw(40rer-)du0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = []
 
